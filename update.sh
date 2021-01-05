1d0a08b40537d337b6b38da0ea16fb304ff297b5
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2021-01-05 14:47:11 +0000

    Add script

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "-------- Updating Package Lists --------" > /var/log/autoupdate.log
+apt-get update >> /var/log/autoupdate.log
+echo "-------- Doing Dist-Upgrade --------" >> /var/log/autoupdate.log
+apt-get dist-upgrade -y >> /var/log/autoupdate.log
+echo "-------- Doing Clean Up --------" >> /var/log/autoupdate.log
+apt-get autoremove -y >> /var/log/autoupdate.log
+echo "\n\nIf you saw any kernel updates, a reboot is required." >> /var/log/autoupdate.log
